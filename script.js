9f038f69157d933e24448401cb256224f4719339
m13corona <mcorona1308@gmail.com>
2024-05-19 20:46:07 -0600

    added more js

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ const quizQuestions = [
     {
         question : "What is JavaScript?",
         options: ["a. A software", "b. A variable", "c. A programming language that adds interactivity to a webpage"],
-        answer: question1.options[2],
+        correct: "c. A programming language that adds interactivity to a webpage"
     },
     
     {
         question : "What is var used for?",
         options : ["a. To declare a variable", "b. to add style in css", "c. it has no functionality"],
-        answer : question2.options[0],
+        correct : "a. To declare a variable"
     },
     
     {
         question : "What does CSS stand for?",
         options : ["Calling of the style sirens", "b. Cheese street style", "Cascading Style Sheets"],
-        answer : question3.options[2],
+        correct : "Cascading Style Sheets"
     },
     
 ];
@@ -56,6 +56,19 @@ function showQuiz() {
         return;
     }
 
-const 
+const thisQuestion = quizQuestions[questionIndex];
+questionText.textContent = thisQuestion.question;
+answerButtons.innerHTML = '';
+thisQuestion.options.forEach(answer => {
+    const button = document.createElement('button');
+    button.textContent = answer;
+    button.addEventListener('click', () => chooseAnswer(answer));
+    answerButtons.appendChild(button);
+});
     
+}
+
+function chooseAnswer(answer) {
+    const thisQuestion = quizQuestions[questionIndex];
+    if (answer === thisQuestion.correct)
 }
